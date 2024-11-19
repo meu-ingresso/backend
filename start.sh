49130fd4ef6644f660abd9fe4645fdcf61a2d3db
Kelvyn Krug <kelvynkrug@gmail.com>
2024-11-19 03:31:27 -0300

    fix

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@ set -e
 
 echo "Configura o NVM"
 export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || { echo "Erro ao carregar o NVM"; exit 1; }
+
+echo "Configura o PATH para Yarn Global"
+export PATH="$PATH:$(yarn global bin)"
 
 echo "Seta a configuração da versão do Node.js"
 nvm use v22.0.0 || exit 11
@@ -18,4 +21,4 @@ echo "Copiando arquivo .env para o diretório de build"
 cp /home/ubuntu/Projetos/shared/conf/.env /home/ubuntu/Projetos/backend/build/
 
 echo "Reiniciando aplicação com PM2"
-pm2 restart meu-ingresso-api || pm2 start build/server.js --name meu-ingresso-api
+pm2 restart meu-ingresso-api || pm2 start /home/ubuntu/Projetos/backend/build/server.js --name meu-ingresso-api
