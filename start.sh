80791be4c94a4cf5333156422fb0d7faae5e9f0e
Kelvyn Krug <kelvynkrug@gmail.com>
2024-11-19 03:21:09 -0300

    fix

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
-#!/bin/bash
-echo "configura o nvm"
-[[ -s /home/ec2-user/.nvm/nvm.sh ]] && . /home/ec2-user/.nvm/nvm.sh
+set -e
 
-echo "seta a configuração da versão do node"
-nvm use v22.0.0 || exit 11
-echo "node version: $(node -v)"
+echo "Configura o NVM"
+[[ -s /home/ubuntu/.nvm/nvm.sh ]] && . /home/ubuntu/.nvm/nvm.sh
 
-yarn install
+echo "Seta a configuração da versão do Node.js"
+
+nvm use 22.0.0 || exit 11
+echo "Node.js versão: $(node -v)"
+
+echo "Instalando dependências..."
+yarn
+
+echo "Compilando aplicação..."
 yarn build
-cd build
-cp ../../../../shared/conf/.env .
-yarn start
+
+echo "Copiando arquivo .env para o diretório de build"
+cp /home/ubuntu/Projetos/shared/conf/.env /home/ubuntu/Projetos/backend/build/
+
+echo "Reiniciando aplicação com PM2"
+pm2 restart meu-ingresso-api || pm2 start build/server.js --name meu-ingresso-api
