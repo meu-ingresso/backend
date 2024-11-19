97d275d71c57e7e948c272cea3873017ec4d4a0e
Kelvyn Krug <kelvynkrug@gmail.com>
2024-11-19 03:35:48 -0300

    fix: emojis

--- start.sh
+++ start.sh
@@ -1,24 +1,28 @@
 set -e
 
-echo "Configura o NVM"
+echo "💡 INICIANDO SCRIPT DE DEPLOY 💡"
+
+echo "🛠️ CONFIGURA O NVM 🛠️"
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" || { echo "Erro ao carregar o NVM"; exit 1; }
 
-echo "Configura o PATH para Yarn Global"
+echo "🛠️ CONFIGURA O PATH PARA YARN GLOBAL 🛠️"
 export PATH="$PATH:$(yarn global bin)"
 
-echo "Seta a configuração da versão do Node.js"
+echo "🆚 SETA A VERSÃO DO NODE.JS 🆚"
 nvm use v22.0.0 || exit 11
 echo "Node.js versão: $(node -v)"
 
-echo "Instalando dependências..."
+echo "⌛⏳ Instalando dependências... ⌛⏳"
 yarn install
 
-echo "Compilando aplicação..."
+echo "⌛⏳ Compilando aplicação... ⌛⏳"
 yarn build
 
-echo "Copiando arquivo .env para o diretório de build"
+echo "✍🏻 COPIANDO ARQUIVO ENV ✍🏻"
 cp /home/ubuntu/Projetos/shared/conf/.env /home/ubuntu/Projetos/backend/build/
 
-echo "Reiniciando aplicação com PM2"
+echo "🚀 REINICIANDO APLICAÇÃO COM PM2 🚀"
 pm2 restart meu-ingresso-api || pm2 start /home/ubuntu/Projetos/backend/build/server.js --name meu-ingresso-api
+
+echo "🚀🚀🚀 DEPLOY FINALIZADO 🚀🚀🚀"
