6f6786cfeb7ca21caf7d49a58b3483cd485248cf
Kelvyn Krug <kelvynkrug@gmail.com>
2025-02-23 20:53:55 -0300

    refactor: update PdvUsersController, Model, Validator, and Migration to support bulk operations and remove soft delete

--- app/Validators/v1/PdvUsersValidator.ts
+++ app/Validators/v1/PdvUsersValidator.ts
@@ -8,15 +8,21 @@ class CreatePdvUsersValidator {
   public reporter = ReportHandler;
 
   public schema = schema.create({
-    pdv_id: schema.string({}, [rules.exists({ table: 'pdvs', column: 'id' })]),
-    user_id: schema.string({}, [rules.exists({ table: 'users', column: 'id' })]),
+    data: schema.array().members(
+      schema.object().members({
+        pdv_id: schema.string({}, [rules.exists({ table: 'pdvs', column: 'id' })]),
+        user_id: schema.string({}, [rules.exists({ table: 'users', column: 'id' })]),
+      })
+    ),
   });
 
   public messages = {
-    'pdv_id.required': 'O campo "pdv_id" é obrigatório.',
-    'pdv_id.exists': 'O pdv especificado não existe.',
-    'user_id.required': 'O campo "user_id" é obrigatório.',
-    'user_id.exists': 'O usuário especificado não existe.',
+    'data.required': 'O campo "data" é obrigatório.',
+    'data.array': 'O campo data deve ser um array.',
+    'data.*.pdv_id.required': 'O campo "pdv_id" é obrigatório.',
+    'data.*.pdv_id.exists': 'O pdv especificado não existe.',
+    'data.*.user_id.required': 'O campo "user_id" é obrigatório.',
+    'data.*.user_id.exists': 'O usuário especificado não existe.',
   };
 }
 
@@ -26,16 +32,22 @@ class UpdatePdvUsersValidator {
   public reporter = ReportHandler;
 
   public schema = schema.create({
-    id: schema.string({}, [rules.exists({ table: 'pdv_users', column: 'id' })]),
-    pdv_id: schema.string.optional({}, [rules.exists({ table: 'pdvs', column: 'id' })]),
-    user_id: schema.string.optional({}, [rules.exists({ table: 'users', column: 'id' })]),
+    data: schema.array().members(
+      schema.object().members({
+        id: schema.string({}, [rules.exists({ table: 'pdv_users', column: 'id' })]),
+        pdv_id: schema.string.optional({}, [rules.exists({ table: 'pdvs', column: 'id' })]),
+        user_id: schema.string.optional({}, [rules.exists({ table: 'users', column: 'id' })]),
+      })
+    ),
   });
 
   public messages = {
-    'id.required': 'O campo "id" é obrigatório.',
-    'id.exists': 'O pdv usuário especificado não existe.',
-    'pdv_id.exists': 'O pdv especificado não existe.',
-    'user_id.exists': 'O usuário especificado não existe.',
+    'data.required': 'O campo "data" é obrigatório.',
+    'data.array': 'O campo data deve ser um array.',
+    'data.*.id.required': 'O campo "id" é obrigatório.',
+    'data.*.id.exists': 'O pdv usuário especificado não existe.',
+    'data.*.pdv_id.exists': 'O pdv especificado não existe.',
+    'data.*.user_id.exists': 'O usuário especificado não existe.',
   };
 }
 
