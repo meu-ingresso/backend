a08e9eb1f0c19f63ff345ab24fe7208920c4ebf2
Kelvyn Krug <kelvynkrug@gmail.com>
2025-02-23 19:19:45 -0300

    refactor: update EventCheckoutFieldsTicketsController and Validator to support bulk operations with nested data schema

--- app/Validators/v1/EventCheckoutFieldsTicketsValidator.ts
+++ app/Validators/v1/EventCheckoutFieldsTicketsValidator.ts
@@ -8,26 +8,23 @@ class CreateEventCheckoutFieldTicketValidator {
   public reporter = ReportHandler;
 
   public schema = schema.create({
-    event_checkout_field_id: schema.string({ trim: true }, [
-      rules.exists({ table: 'event_checkout_fields', column: 'id' }),
-      rules.unique({
-        table: 'event_checkout_fields_tickets',
-        column: 'event_checkout_field_id',
-        where: {
-          ticket_id: this.context.request.input('ticket_id'),
-          deleted_at: null,
-        },
-      }),
-    ]),
-    ticket_id: schema.string({ trim: true }, [rules.exists({ table: 'tickets', column: 'id' })]),
+    data: schema.array().members(
+      schema.object().members({
+        event_checkout_field_id: schema.string({ trim: true }, [
+          rules.exists({ table: 'event_checkout_fields', column: 'id' }),
+        ]),
+        ticket_id: schema.string({ trim: true }, [rules.exists({ table: 'tickets', column: 'id' })]),
+      })
+    ),
   });
 
   public messages = {
-    'event_checkout_field_id.required': 'O campo "event_checkout_field_id" é obrigatório.',
-    'event_checkout_field_id.exists': 'O "event_checkout_field_id" fornecido não existe.',
-    'event_checkout_field_id.unique': 'Essa combinação de "event_checkout_field_id" e "ticket_id" já existe.',
-    'ticket_id.required': 'O campo "ticket_id" é obrigatório.',
-    'ticket_id.exists': 'O "ticket_id" fornecido não existe.',
+    'data.required': 'O campo "data" é obrigatório.',
+    'data.array': 'O campo "data" deve ser um array.',
+    'data.*.event_checkout_field_id.required': 'O campo "event_checkout_field_id" é obrigatório.',
+    'data.*.event_checkout_field_id.exists': 'O "event_checkout_field_id" fornecido não existe.',
+    'data.*.ticket_id.required': 'O campo "ticket_id" é obrigatório.',
+    'data.*.ticket_id.exists': 'O "ticket_id" fornecido não existe.',
   };
 }
 
